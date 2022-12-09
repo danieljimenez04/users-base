ca749776a55f3dc4b2110aa0de3c569e2764804e
danieljimenez04 <dajimeneza@uni.pe>
2022-12-08 23:02:30 -0500

    subiendo repo

---
+++ src/auth/auth.router.js
@@ -0,0 +1,7 @@
+const postLogin=require('./auth.services')
+
+const router=require('express').Router()
+
+router.post('/login',postLogin)
+
+module.exports=router
--- src/users/users.router.js
+++ src/users/users.router.js
@@ -2,11 +2,14 @@ const router = require('express').Router()
 
 const userServices = require('./users.services')
 
+const JwtPassport=require('../middleware/auth.middleware')
+
+
 router.get("/", userServices.getAllUsers) //? /api/v1/users
 router.post("/", userServices.postUser) //? /api/v1/users
 
 router.get("/:id", userServices.getUserById) //? /api/v1/users/:id
-router.patch('/:id', userServices.patchUser) //? /api/v1/users/:id
-router.delete('/:id', userServices.deleteUser) //? /api/v1/users/:id
+router.patch('/:id',JwtPassport.authenticate('jwt',{session:false}) ,userServices.patchUser) //? /api/v1/users/:id
+router.delete('/:id',JwtPassport.authenticate('jwt',{session:false}) ,userServices.deleteUser) //? /api/v1/users/:id
 
 module.exports = router
